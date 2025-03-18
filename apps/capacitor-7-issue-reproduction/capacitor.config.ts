e8fa9dfbf81bc9e65901a6e182fa00e6b592adac
Dmitry Pashkevich <dp@zeemo.eu>
2025-03-18 16:34:35 +0100

    add android

---
+++ apps/capacitor-7-issue-reproduction/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-7-issue-reproduction',
+  webDir: '../../dist/apps/capacitor-7-issue-reproduction/browser'
+};
+
+export default config;
